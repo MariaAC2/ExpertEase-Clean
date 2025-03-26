8d73d4478c2e0e1221c77ed0db21a60ac5a188a5
Maria Chirnogeanu <maria.chirnogeanu@stud.acs.upb.ro>
2025-03-26 20:22:20 +0200

    Added angular part of project

--- ExpertEase.Application/DataTransferObjects/UserUpdateDTO.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Example.Application.DataTransferObjects;
-
-/// <summary>
-/// This DTO is used to update a user, the properties besides the id are nullable to indicate that they may not be updated if they are null.
-/// </summary>
-public record UserUpdateDTO(Guid Id, string? Name = null, string? Password = null);
--- ExpertEase.Infrastructure/Authorization/UserClaims.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ExpertEase.Authorization;
-
-/// <summary>
-/// This record is used to store the claims extracted from the JWT
-/// </summary>
-public record UserClaims(Guid Id, string? Name, string? Email);
